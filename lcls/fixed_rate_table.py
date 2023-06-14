9db2bdfba6ffe581a790dc8990c821180e6292bf
Matt Weaver <weaver@slac.stanford.edu>
2023-06-14 07:48:27 -0700

    Add option nth harmonic selection

--- lcls/fixed_rate_table.py
+++ lcls/fixed_rate_table.py
@@ -1,23 +1,45 @@
+import argparse
 import sys
 import itertools
 import numpy as np
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='simple validation printing')
+    parser.add_argument("-f", "--factor", required=False , type=int, default=1, help="only show Nth sub-harmonics")
+    args = parser.parse_args()
+
     factors = [2,2,2,2,5,5,5,5,7,13]  # product is 910,000
-    iters = [itertools.combinations(factors,i+1) for i in range(len(factors))]
+    subfactors = []
+    remfactors = []
+    sf = args.factor
+    for f in factors:
+        d = sf/f
+        if int(d)==d:
+            sf = d
+            remfactors.append(f)
+        else:
+            subfactors.append(f)
+
+    rf = tuple(remfactors)
+
+    iters = [itertools.combinations(subfactors,i+1) for i in range(len(subfactors))]
     f = set()
     d = {}
     for i in iters:
         for c in i:
-            q = np.prod(np.array(c))
+            q = np.prod(np.array(c))*args.factor
             f.add(q)
-            d[q] = c
+            d[q] = c+rf
 
-    f.add(1)
-    d[1] = 1
+    f.add(args.factor)
+    d[args.factor] = rf
 
     base = 1300.e6/1400.
-    print(' rate, Hz  | factor | factors')
-    for q in sorted(f):
-        print(' {:6d}     {:6d}   {}'.format(int(base/float(q)),q,d[q]))
-        
+    if args.factor > 1:
+        print(' rate, Hz  | subfactor | factor | factors')
+        for q in sorted(f):
+            print(' {:6d}        {:6d}   {:6d}   {}'.format(int(base/float(q)),int(q/args.factor),q,d[q]))
+    else:
+        print(' rate, Hz  | factor | factors')
+        for q in sorted(f):
+            print(' {:6d}     {:6d}   {}'.format(int(base/float(q)),q,d[q]))
